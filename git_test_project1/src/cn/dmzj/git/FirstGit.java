f31c86b4b4343f0ce6ebb33457377ef1a281499d
Jia <3215035381@qq.com>
2023-08-07 11:30:56 +0800

    增加了一个空的test方法

---
+++ git_test_project1/src/cn/dmzj/git/FirstGit.java
@@ -0,0 +1,17 @@
+package cn.dmzj.git;
+
+/**
+ * @Author：DaiMuZiJia
+ * @name：FirstGit
+ * @Date：2023/8/7 0007  10:17
+ */
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+    public void test(){
+
+    }
+}
